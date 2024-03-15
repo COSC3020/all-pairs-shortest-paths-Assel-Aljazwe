942dc14d1e9068adc4fde9531d22a8b9619e9664
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-03-15 04:11:28 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
-function allPairsShortestPaths(graph) {
-    return [];
+function floydWarshall(graph) {
+    const numVertices = graph.length;
+    const dist = Array.from({ length: numVertices }, () => Array(numVertices).fill(Infinity));
+
+    // Initialize distances using graph
+    for (let i = 0; i < numVertices; i++) {
+        dist[i][i] = 0;
+        for (const [j, weight] of graph[i].entries()) {
+            if (weight !== Infinity && weight !== undefined) dist[i][j] = weight;
+        }
+    }
+
+    // Floyd-Warshall algorithm
+    for (let k = 0; k < numVertices; k++) {
+        for (let i = 0; i < numVertices; i++) {
+            for (let j = 0; j < numVertices; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    return dist;
 }
+
+module.exports = { floydWarshall };
+
